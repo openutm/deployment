867fa4834f62efa7b643ee6cdfe220d8b4ce1384
Roman Pszonka <roman@pszonka.org>
2024-02-01 23:56:07 +0000

    cleanup templates for cert and ingress

--- generate-from-templates.sh
+++ generate-from-templates.sh
@@ -4,7 +4,7 @@ export ACME_CONTACT_EMAIL="info+openutm@pingzone.ie"
 for file_name in issuer certificate ingress; do
     (
         echo "cat <<EOF >${file_name}.yaml"
-        cat ${file_name}-template.yaml
+        cat templates/${file_name}-template.yaml
         echo "EOF"
     ) >${file_name}-temp.yaml
     . ${file_name}-temp.yaml
